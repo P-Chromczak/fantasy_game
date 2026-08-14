7cfcbb7eefd59ac8e657c01599c085becd1edf69
Paweł <pchromczak@gmail.com>
2026-08-14 19:12:36 +0200

    Add scroll support to login screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -67,99 +67,110 @@ class _LoginScreenState extends State<LoginScreen> {
     return Scaffold(
       body: Stack(
         children: [
+
           const AuthBackground(),
 
-          Center(
-            child: AuthGlassCard(
-              child: Form(
-                key: _formKey,
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    const Text(
-                      "Welcome Back, Hero",
-                      style: TextStyle(
-                        fontSize: 22,
-                        fontWeight: FontWeight.w200,
-                        color: Color.fromARGB(255, 207, 207, 207),
+          SafeArea(
+            child: SingleChildScrollView(
+              child: ConstrainedBox(
+                constraints: BoxConstraints(
+                  minHeight: MediaQuery.of(context).size.height,
+                ),
+                child: Center(
+                  child: AuthGlassCard(
+                    child: Form(
+                      key: _formKey,
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+
+                          const Text(
+                            "Welcome Back, Hero",
+                            style: TextStyle(
+                              fontSize: 22,
+                              fontWeight: FontWeight.w200,
+                              color: Color.fromARGB(255, 207, 207, 207),
+                            ),
+                          ),
+
+                          const SizedBox(height: 15),
+
+                          AuthFormField(
+                            controller: _nameController,
+                            validator: validateName,
+                            labelText: "Name",
+                            hintText: "Enter your name",
+                          ),
+
+                          const SizedBox(height: 10),
+
+                          AuthFormField(
+                            controller: _passwordController,
+                            validator: validatePassword,
+                            obscureText: true,
+                            labelText: "Password",
+                            hintText: "Enter your Password",
+                          ),
+
+                          const SizedBox(height: 10),
+
+                          AuthButton(
+                            buttonText: "Log in",
+                            onPressed: login
+                          ),
+
+                          const SizedBox(height: 30),
+
+                          OAuthLoginButton(
+                            buttonText: "Log in with Google",
+                            buttonImage: "assets/images/google.svg",
+                          ),
+
+                          const SizedBox(height: 5),
+
+                          OAuthLoginButton(
+                            buttonText: "Log in with Discord",
+                            buttonImage: "assets/images/discord.svg",
+                          ),
+
+                          const SizedBox(height: 5),
+
+                          OAuthLoginButton(
+                            buttonText: "Log in with Apple",
+                            buttonImage: "assets/images/apple.svg",
+                          ),
+
+                          const SizedBox(height: 5),
+
+                          OAuthLoginButton(
+                            buttonText: "Log in with Facebook",
+                            buttonImage: "assets/images/facebook.svg",
+                          ),
+
+                          const SizedBox(height: 20),
+
+                          const Text(
+                            "Don't have an account?",
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Color.fromARGB(255, 207, 207, 207),
+                            ),
+                          ),
+
+                          InkWell(
+                            onTap: () {},
+                            child: const Text(
+                              "Sign up",
+                              style: TextStyle(
+                                fontSize: 20,
+                                color: Color(0xFFFDE68A),
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
                     ),
-
-                    const SizedBox(height: 15),
-
-                    AuthFormField(
-                      controller: _nameController,
-                      validator: validateName,
-                      labelText: "Name",
-                      hintText: "Enter your name",
-                    ),
-
-                    const SizedBox(height: 8),
-
-                    AuthFormField(
-                      controller: _passwordController,
-                      validator: validatePassword,
-                      obscureText: true,
-                      labelText: "Password",
-                      hintText: "Enter your Password",
-                    ),
-
-                    const SizedBox(height: 10),
-
-                    AuthButton(
-                      buttonText: "Log in",
-                       onPressed: login
-                    ),
-
-                    const SizedBox(height: 30),
-
-                    OAuthLoginButton(
-                      buttonText: "Log in with Google",
-                      buttonImage: "assets/images/google.svg",
-                    ),
-
-                    const SizedBox(height: 5),
-
-                    OAuthLoginButton(
-                      buttonText: "Log in with Discord",
-                      buttonImage: "assets/images/discord.svg",
-                    ),
-
-                    const SizedBox(height: 5),
-
-                    OAuthLoginButton(
-                      buttonText: "Log in with Apple",
-                      buttonImage: "assets/images/apple.svg",
-                    ),
-
-                    const SizedBox(height: 5),
-
-                    OAuthLoginButton(
-                      buttonText: "Log in with Facebook",
-                      buttonImage: "assets/images/facebook.svg",
-                    ),
-
-                    const SizedBox(height: 20),
-
-                    const Text(
-                      "Don't have an account?",
-                      style: TextStyle(
-                        fontSize: 16,
-                        color: Color.fromARGB(255, 207, 207, 207),
-                      ),
-                    ),
-
-                    InkWell(
-                      onTap: () {},
-                      child: const Text(
-                        "Sign up",
-                        style: TextStyle(
-                          fontSize: 20,
-                          color: Color(0xFFFDE68A),
-                        ),
-                      ),
-                    ),
-                  ],
+                  ),
                 ),
               ),
             ),
