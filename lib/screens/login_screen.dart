2b3e70a3228f02c09df13e0a7a2f6bb2e86e5cfb
Paweł <pchromczak@gmail.com>
2026-08-14 16:51:34 +0200

    Add glass card

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+
 import '../widgets/auth/auth_background.dart';
+import '../widgets/auth/auth_glass_card.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -12,7 +14,29 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const AuthBackground(),
+      body: Stack(
+        children: [
+          const AuthBackground(),
+
+          Center(
+            child: AuthGlassCard(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  const Text(
+                    "Welcome Back, Hero",
+                    style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.w200,
+                      color: Color.fromARGB(255, 207, 207, 207),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
